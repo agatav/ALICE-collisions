f66ff8b58c02f4416b1e32c225a8eb9e0c9eb2ce
agatav <ahatavaliukevich@gmail.com>
2018-01-10 21:42:57 +0100

    Correct tracks drawing, small changes in pdf. TODO colors of tracks;

--- Headers/Track.h
+++ Headers/Track.h
@@ -10,15 +10,35 @@ using namespace std;
 ifstream ifs("/home/agata/Documents/tracks/track.json");
 
 class Track{
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     bool isInited;
-    bool gammaCorrection;
 
+private: int numsToDraw;
 
 public:
     void init(){
         vector<GLfloat> tracks;
-        getTracks(tracks);
+        vector<GLuint> indices;
+        int indicator = 0;
+
+        Json::Value objectTracks;
+        Json::Reader readerTracks;
+        readerTracks.parse(ifs,objectTracks);
+
+        for (Json::Value::iterator it = objectTracks["fTracks"].begin(); it != objectTracks["fTracks"].end(); ++it){
+
+            for (int i = 0; i < (*it)["fPolyX"].size(); i++){
+                tracks.push_back((*it)["fPolyX"][i].asFloat());
+                tracks.push_back((*it)["fPolyY"][i].asFloat());
+                tracks.push_back((*it)["fPolyZ"][i].asFloat());
+                tracks.push_back((*it)["fMass"].asFloat());
+                tracks.push_back((*it)["fMass"].asFloat());
+                indices.push_back(indicator);
+                indicator++;
+            }
+            indices.push_back(GL_PRIMITIVE_RESTART_FIXED_INDEX);
+        }
+
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
         glBindVertexArray(VAO);
@@ -29,6 +49,11 @@ public:
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
         glBindVertexArray(1);
 
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), &indices[0], GL_STATIC_DRAW);
+
+        numsToDraw = indices.size();
         isInited = true;
     }
 
@@ -42,10 +67,13 @@ public:
         if (VAO) {
             glDeleteVertexArrays(1, &VAO);
         }
-
+        if(EBO) {
+            glDeleteBuffers(1, &EBO);
+        }
         isInited = false;
         VAO = 0;
         VBO = 0;
+        EBO = 0;
     }
 
     void draw()
@@ -55,24 +83,10 @@ public:
         }
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_LINE_STRIP_ADJACENCY, 0, 348);
-    }
-private:
-
-    void getTracks(vector<GLfloat> &tracks){
-        Json::Value objectTracks;
-        Json::Reader readerTracks;
-        readerTracks.parse(ifs,objectTracks);
-        for (Json::Value::iterator it = objectTracks["fTracks"].begin(); it != objectTracks["fTracks"].end(); ++it){
-
-            for (int i = 0; i < (*it)["fPolyX"].size(); i++){
-                tracks.push_back((*it)["fPolyX"][i].asFloat());
-                tracks.push_back((*it)["fPolyY"][i].asFloat());
-                tracks.push_back((*it)["fPolyZ"][i].asFloat());
-                tracks.push_back((*it)["fMass"].asFloat());
-                tracks.push_back(1.0f);
-            }
-        }
+        glEnable(GL_PRIMITIVE_RESTART);
+        glPrimitiveRestartIndex(GL_PRIMITIVE_RESTART_FIXED_INDEX);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_LINE_STRIP_ADJACENCY, numsToDraw, GL_UNSIGNED_INT,  (void*)0 );
     };
 };
 #endif
